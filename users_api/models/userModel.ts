6a2967987992823454e33900d8cab3f16dea8aad
murilo-scapim <mhscapim@gmail.com>
2023-03-22 22:09:24 -0300

    Aula 22/03

---
+++ users_api/models/userModel.ts
@@ -0,0 +1,20 @@
+import { Pool } from 'mysql2/promise';
+
+interface User {
+  id?: number;
+  username: string;
+  password: string;
+}
+
+class UserModel {
+  public connection: Pool;
+
+  constructor(connection: Pool) {
+    this.connection = connection;
+  }
+
+  public async getAll(): Promise<User[]> {
+    const users = await this.connection.execute('SELECT * FROM users');
+    console.log(users);
+  }
+}
